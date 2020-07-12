4546c605d8c2dcea76a5d2619f0ce51e40b4b6a0
ALmsq <alexander.mosquea3@gmail.com>
2020-07-12 16:22:05 -0400

    fixed route issue

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,230 +1,92 @@
-// import React, { useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import userActions from '../redux/actions';
-// import { useHistory } from "react-router-dom"
-
-// const Login = props => {
-//   // initializing dispatch
-//   const dispatch = useDispatch();
-//   // Setting up local state using the useState hook
-//   const [loginForm, setLoginForm] = useState({
-//     username: '',
-//     password: ''
-//   });
-
-//   let history = useHistory();
-//   // controlled form functions
-//   const handleSubmit = e => {
-//     e.preventDefault();
-//     dispatch(userActions.loginUserToDB(loginForm));
-//     history.push("/home");
-//   };
-
-//   const handleChange = e =>
-//     setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
-
-//   // Destructuring keys from our local state to use in the form
-//   const { username, password } = loginForm;
-
-//   console.log(props)
-
-//   // Component code
-//   return (
-//       console.log(loginForm),
-//     <form onSubmit={handleSubmit}>
-//       <h1>Login Page</h1>
-//       <input
-//         type="text"
-//         name="username"
-//         value={username}
-//         onChange={handleChange}
-//         placeholder="Username"
-//       />
-//       <input
-//         type="password"
-//         name="password"
-//         value={password}
-//         onChange={handleChange}
-//         placeholder="Password"
-//       />
-//       <input type="submit" />
-//     </form>
-//   );
-// };
-
-// export default Login;
-
-/////////////////////
-
-import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import userActions from '../redux/actions';
-import { useHistory } from "react-router-dom"
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import Place from './Place'
+import Profile from './Profile'
+import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
+import { Grid, Image } from 'semantic-ui-react'
+import Pizza from '../Pizza'
+import '../HomePage.css'
+import styled from 'styled-components'
+import { SCards } from '../Style.js'
 
 
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-// import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+// class HomePage extends Component {
+//     render() {
+        
+//         return (
+//             <div>
+//                 <h1>Featured Spots</h1>
+//                 <Place/>
+//             </div>
+//         )
+//     }
+// }
+    
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-        <Link color="inherit" href="https://material-ui.com/">
-          Your Website
-        </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+const HomePage = () => {
+        const username = useSelector(state => state.login.username)
+        const places = useSelector(state => state.place)
+        const query = useSelector(state => state.query)
+        const newQuery = useSelector(state => state.newQuery)
+        console.log(places)
+        console.log(query)
+        console.log(newQuery)
+
+        const text = username ? (
+            <h1>{username} is currently logged in</h1>
+          ) : (
+            <h1>Nobody is logged in</h1>
+          );
+          console.log(decodeURI(query))
+
+          const decodedQuery = useState(decodeURI(newQuery))
+          console.log(decodedQuery)
+          
+          
+          localStorage.setItem('query', JSON.stringify(decodedQuery))
+          const placeQuery = query ? (<h1>{decodeURI(query)}</h1>) : (<h1>{query}</h1>)
+          
+
+        const generateCards = () => {
+            return places.map(c => <Place c={c} key={c.id} />);
+          };
+
+        const generateProfileCards = () => {
+            return places.map(c => <Profile c={c} key={c.id} />);
+          };
+
+        return(
+            // <div>
+            //     <h1>Featured Spots</h1>
+            //     {generateCards()}
+
+            // </div>
+            <div>
+            <Pizza/>
+            <h1 style={{textAlign: 'center'}}>Restaurants near {placeQuery}</h1>
+            <div class = 'home-page'>
+              <SCards>
+            {/* <Grid columns={3} divided> 
+                <Grid.Row>
+                        {generateCards()}
+                        {/* <div>{text}</div> */}
+                        {/* {generateProfileCards()} */}
+                {/* </Grid.Row>
+            </Grid> */} 
+              {generateCards()}
+              </SCards>
+            </div>
+            </div>
+        )
 }
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    height: '100vh',
-  },
-  image: {
-    backgroundImage: 'url(https://source.unsplash.com/1600x900/?restaurants,food)',
-    backgroundRepeat: 'no-repeat',
-    backgroundColor:
-      theme.palette.type === 'dark' ? theme.palette.grey[900] : theme.palette.grey[50],
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
-
-const Login = props => {
-  const classes = useStyles();
-
-  //  initializing dispatch
-  const dispatch = useDispatch();
-  // Setting up local state using the useState hook
-  const [loginForm, setLoginForm] = useState({
-    username: '',
-    password: ''
-  });
-
-  let history = useHistory();
-  // controlled form functions
-  const handleSubmit = e => {
-    // debugger
-    e.preventDefault();
-    dispatch(userActions.loginUserToDB(loginForm))
-    history.push('/')
-  }
-
-  const handleChange = e =>
-    setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
-
-  // Destructuring keys from our local state to use in the form
-  const { username, password } = loginForm;
+// const mapStateToProps = state => {
+//     return {
+//         place: state.allPlaces.place
+//     }
+// }
 
 
 
-  return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Log in
-          </Typography>
-        <form className={classes.form} noValidate onSubmit={handleSubmit}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="login"
-              label="Login"
-              name="username"
-              autoComplete="login"
-
-              
-              value={username}
-              onChange={handleChange}
-
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-
-              
-              value={password}
-              onChange={handleChange}
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Log In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                {/* <Link href="#" variant="body2">
-                  Forgot password?
-                </Link> */}
-              </Grid>
-              <Grid item>
-                <Link to='/signup' variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-            <Box mt={5}>
-              {/* <Copyright /> */}
-            </Box>
-          </form>
-        </div>
-      </Grid>
-    </Grid>
-  );
-}
-export default Login
+// export default connect(mapStateToProps)(HomePage)
+export default HomePage
